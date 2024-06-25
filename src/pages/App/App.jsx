fa19e7ed9ba125d90c3eb562a4c90d4bf50cd97c
jesstheleftie <jessetang57@hotmail.com>
2024-06-24 22:32:38 -0700

    first commit

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {useState} from "react";
+
+function App() {
+const [user, setUser] = useState(null);
+
+  return (
+    <main className="App">
+      App2
+
+    </main>
+  );
+}
+
+export default App;
